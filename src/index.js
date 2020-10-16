d295ddb2d7ca3f0ceb4007126f64b8231400001e
kjendrzyca <kjendrzyca@users.noreply.github.com>
2020-10-16 22:54:07 +0200

    Upgrade everything and add Eslint config

--- src/index.js
+++ src/index.js
@@ -1,52 +1,66 @@
 import React from 'react'
-import { render } from 'react-dom'
-import { createStore, applyMiddleware } from 'redux'
-import { Provider } from 'react-redux'
-import createSagaMiddleware from 'redux-saga'
+import {render} from 'react-dom'
+import {
+  createStore,
+  // applyMiddleware
+} from 'redux'
+import {Provider} from 'react-redux'
+// import createSagaMiddleware from 'redux-saga'
 
 import reducer from './reducers'
-import pushToServerSaga from './pushToServerSaga'
+// import pushToServerSaga from './pushToServerSaga'
 import api from './api'
 import generateTree from './generateTree'
-import Node from './containers/Node'
+import ConnectedNode from './containers/Node'
 
 import './index.css'
 
-async function seedApi () {
+// TODO: server sync
+// eslint-disable-next-line no-unused-vars
+async function seedApi() {
   const tree = generateTree()
   const data = await api.getAll()
   if (data.length) {
     return data
   }
 
-  for (let node of Object.values(tree)) {
+  // eslint-disable-next-line no-restricted-syntax
+  for (const node of Object.values(tree)) {
+    // eslint-disable-next-line no-await-in-loop
     await api.create(node)
   }
 
-  return await api.getAll()
+  return api.getAll()
 }
 
-async function run () {
-  const data = await seedApi()
-  const reducedData = data.reduce(function(map, obj) {
-      map[obj.id] = obj
-      return map
+async function run() {
+  // const data = await seedApi()
+  const data = Object.values(generateTree())
+
+  console.log({data})
+
+  const reducedData = data.reduce((map, obj) => {
+    // eslint-disable-next-line no-param-reassign
+    map[obj.id] = obj
+    return map
   }, {})
 
-  const sagaMiddleware = createSagaMiddleware()
+  console.log({reducedData})
+
+  // const sagaMiddleware = createSagaMiddleware()
   const store = createStore(
     reducer,
     reducedData,
-    applyMiddleware(sagaMiddleware)
+    // applyMiddleware(sagaMiddleware)
   )
 
-  sagaMiddleware.run(pushToServerSaga)
+  // sagaMiddleware.run(pushToServerSaga)
 
   render(
     <Provider store={store}>
-      <Node id={0} />
+      <ConnectedNode id={0} />
     </Provider>,
-    document.getElementById('root')
+    document.getElementById('root'),
   )
 }
 
